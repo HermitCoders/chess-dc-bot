47681f80810ad95c1eac82c7537a3841d5230513
acwikla <arkadiusz.cwikla98@gmail.com>
2024-02-24 12:09:08 +0100

    add basic hello callback

--- chess_app.py
+++ chess_app.py
@@ -19,5 +19,15 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_message(msg):
+    # ignore messages from ourselfes
+    if msg.author == client.user:
+        return
+    
+    if msg.content.startswith('!siema'):
+        await msg.channel.send('Siema eniu!')
+        print("Hello message sent")
+
 # launch app with saved token
 client.run(TOKEN)
