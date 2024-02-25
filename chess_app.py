4202fff39c346f7751bedfcae063c2f0e66f5e15
acwikla <arkadiusz.cwikla98@gmail.com>
2024-02-25 02:17:04 +0100

    connect music cog to bot

--- chess_app.py
+++ chess_app.py
@@ -4,6 +4,7 @@ import asyncio
 from dotenv import load_dotenv
 from discord.ext import commands
 from chess_cogs.greeting import Greeting
+from chess_cogs.music import Music
 
 # load Discord bot token from .env file
 load_dotenv()
@@ -14,23 +15,26 @@ print(TOKEN)
 intents = discord.Intents.default()
 intents.message_content = True
 
-# create not so-basic bot connection to Discord
 bot = commands.Bot(
     command_prefix=commands.when_mentioned_or("!"),
-    description="DJ SZACH Z ODSŁONY",
-    intents=intents
+    description='DJ SZACH Z ODSŁONY WJEŻDZA NA SALONY',
+    intents=intents,
 )
 
-# register bot callbacks
+
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
 
 async def main():
     async with bot:
         # add cogs
         await bot.add_cog(Greeting(bot))
+        await bot.add_cog(Music(bot))
         # launch app with saved token
         await bot.start(TOKEN)
+
 
 asyncio.run(main())
